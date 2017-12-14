c14988b3d32479ab045e2e903997851644aa2b55
songzy <songzy.thu@qq.com>
2017-12-15 00:47:17 +0800

    lda topic feature added

--- XGB.py
+++ XGB.py
@@ -74,6 +74,25 @@ train_y = train_df['author'].map(author_mapping_dict)
 train_id = train_df['id'].values
 test_id = test_df['id'].values
 
+from sklearn.decomposition import NMF, LatentDirichletAllocation
+
+### Fit transform the tfidf vectorizer ###
+tfidf_vec = CountVectorizer(stop_words='english', ngram_range=(1,3))
+full_tfidf = tfidf_vec.fit_transform(train_df['text'].values.tolist() + test_df['text'].values.tolist())
+train_tfidf = tfidf_vec.transform(train_df['text'].values.tolist())
+test_tfidf = tfidf_vec.transform(test_df['text'].values.tolist())
+
+no_topics = 100 
+lda = LatentDirichletAllocation(n_topics=no_topics, max_iter=5, learning_method='online', learning_offset=50.,random_state=0).fit(full_tfidf)
+train_lda = pd.DataFrame(lda.transform(train_tfidf))
+test_lda = pd.DataFrame(lda.transform(test_tfidf))
+    
+train_lda.columns = ['lda_'+str(i) for i in range(no_topics)]
+test_lda.columns = ['lda_'+str(i) for i in range(no_topics)]
+train_df = pd.concat([train_df, train_lda], axis=1)
+test_df = pd.concat([test_df, test_lda], axis=1)
+del full_tfidf, train_tfidf, test_tfidf, train_lda, test_lda
+
 # load the GloVe vectors in a dictionary:
 
 def loadWordVecs():
@@ -452,7 +471,6 @@ test_svd.columns = ['svd_word_'+str(i) for i in range(n_comp)]
 train_df = pd.concat([train_df, train_svd], axis=1)
 test_df = pd.concat([test_df, test_svd], axis=1)
 del full_tfidf, train_tfidf, test_tfidf, train_svd, test_svd
-
 
 ### Fit transform the count vectorizer ###
 tfidf_vec = CountVectorizer(stop_words='english', ngram_range=(1,3))

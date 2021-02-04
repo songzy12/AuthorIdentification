3e1abcfc22d9a430c31972845b69e9ffde9844eb
Zhengyang Song <songzy_thu@163.com>
2021-02-05 01:24:43 +0800

    Playground Voting Classifier.

--- playground/voting_classifier.py
+++ playground/voting_classifier.py
@@ -13,6 +13,15 @@ from sklearn.naive_bayes import BernoulliNB, MultinomialNB
 from sklearn.linear_model import SGDClassifier, LogisticRegression
 from sklearn.calibration import CalibratedClassifierCV
 
+# Data.
+train = pd.read_csv('../input/train.csv')
+vectorizer = TfidfVectorizer(
+    token_pattern=r'\w{1,}', sublinear_tf=True, ngram_range=(1, 2))
+X_train = vectorizer.fit_transform(train.text.values)
+authors = ['MWS', 'EAP', 'HPL']
+y_train = train.author.apply(authors.index).values
+
+# Train.
 models = [('MultiNB', MultinomialNB(alpha=0.03)),
           ('Calibrated MultiNB', CalibratedClassifierCV(
               MultinomialNB(alpha=0.03), method='isotonic')),
@@ -22,16 +31,12 @@ models = [('MultiNB', MultinomialNB(alpha=0.03)),
               SGDClassifier(loss='modified_huber', alpha=1e-4,
                             max_iter=10000, tol=1e-4), method='sigmoid')),
           ('Logit', LogisticRegression(C=30))]
-
-train = pd.read_csv('./input/train.csv')
-vectorizer=TfidfVectorizer(token_pattern=r'\w{1,}', sublinear_tf=True, ngram_range=(1,2))
-clf = VotingClassifier(models, voting='soft', weights=[3,3,3,1,1])
-X_train = vectorizer.fit_transform(train.text.values)
-authors = ['MWS','EAP','HPL']
-y_train = train.author.apply(authors.index).values
+clf = VotingClassifier(models, voting='soft', weights=[3, 3, 3, 1, 1])
 clf.fit(X_train, y_train)
 
-test = pd.read_csv('./input/test.csv', index_col=0)
+# Infer.
+test = pd.read_csv('../input/test.csv', index_col=0)
 X_test = vectorizer.transform(test.text.values)
 results = clf.predict_proba(X_test)
-pd.DataFrame(results, index=test.index, columns=authors).to_csv('result.csv')
+pd.DataFrame(results, index=test.index, columns=authors).to_csv(
+    '../output/submission.csv')
